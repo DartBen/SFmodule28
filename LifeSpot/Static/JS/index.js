3911e94490f43732cf795f4c11b353f4ee5a2913
DESKTOP-BBDMNJN\Dima <ryabchikk@rambler.ru>
2023-03-26 21:51:31 +0300

    Измените ваш код в index.js таким образом, чтобы кроме проверки на возраст посетителя мы также сохраняли некоторую информацию о нём: возраст, время начала сессии, строку User Agent.

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,3 +1,19 @@
-﻿//b = prompt("Введите X: ", "5");
-//let a = b * 3.0;
-//alert(a);
+﻿let userInfo=new Map();
+let UserAgent = window.navigator.userAgent;
+userInfo.set('UserAgent', UserAgent);
+age = prompt("Введите свой возраст.", "");
+if (age >= 18) { alert("Приветствуем на LifeSpot! " + new Date().toLocaleString()); }
+else {
+    alert("Данный сайт не предназначен для лиц младше 18 лет!");
+    window.location.href = "http://www.google.com";
+};
+userInfo.set('Age', age);
+let timeOfSesionStart = new Date().toLocaleString()
+userInfo.set('TimeOfSesionStart',timeOfSesionStart);
+
+// Вывод в консоль
+for (let result of userInfo){
+    console.log(result)
+}
+//возраст, время начала сессии, строку User Agent
+
